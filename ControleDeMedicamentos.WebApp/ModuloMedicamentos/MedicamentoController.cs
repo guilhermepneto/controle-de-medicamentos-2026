8bea8ed0f232f8e8177824fe3a9a96b00897919e
guilhermepneto <gui.pereiraneto@hotmail.com>
2026-07-23 19:40:04 -0300

    add listagem de medicamentos

---
+++ ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
@@ -0,0 +1,26 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.Arquivos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeMedicamentos.WebApp.ModuloMedicamentos;
+
+public sealed class MedicamentoController : Controller
+{
+    private readonly RepositorioMedicamentoEmArquivo repositorio;
+
+    public MedicamentoController()
+    {
+        ContextoJson contexto = new ContextoJson();
+
+        contexto.Carregar();
+
+        repositorio = new RepositorioMedicamentoEmArquivo(contexto);
+    }
+
+    [HttpGet]
+    public ActionResult Listar()
+    {
+        List<Medicamento> medicamentos = repositorio.SelecionarTodos();
+
+        return View(medicamentos);
+    }
+}
